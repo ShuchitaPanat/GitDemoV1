0513ade9b0126471b9ef2876cb3a21cc94c6b188
shuch <shuch@Suchi>
2022-01-03 17:44:57 +0530

    Third code updated

---
+++ GitDemoV1/src/Git_Demo_Practice_V1/Demo_3.java
@@ -0,0 +1,11 @@
+package Git_Demo_Practice_V1;
+
+public class Demo_3 {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.gecko.driver", "./Drivers/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+	}
+
+}
